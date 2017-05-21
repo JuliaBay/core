7c99c5f0f1bc40c099230c1c717349d60fda8bb3
JuliaBay <juliabay54@gmail.com>
2017-05-22 01:54:29 +0500

    so

---
+++ src/7lab/MiddlewareClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Builder;
+
+namespace _7lab
+{
+    public class MiddlewareClass
+    {
+
+
+        private readonly RequestDelegate _next;
+        TimeService _timeService;
+
+        public MiddlewareClass(RequestDelegate next, TimeService timeService)
+        {
+            _next = next;
+            _timeService = timeService;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Path.Value.ToLower() == "/time")
+            {
+                await context.Response.WriteAsync($"Текущее время: {_timeService?.GetTime()}");
+            }
+            else
+            {
+                await _next.Invoke(context);
+            }
+        }
+    }
+
+    public static class TimerExtensions
+    {
+        public static IApplicationBuilder UseTimer(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<MiddlewareClass>();
+        }
+    }
+}
+public class TimeService
+{
+    public string GetTime() => System.DateTime.Now.ToString("hh:mm:ss");
+}
